0c9cc5fd631f2fe295662c7ef43e455bf77db75c
Jafet Diaz <79779934+jaberwooc@users.noreply.github.com>
2021-12-10 15:22:42 -0800

    Create pausar.s

---
+++ Codigos/pausar.s
@@ -0,0 +1,53 @@
+
+
+@ ---------------------------------------
+@	Data Section
+@ ---------------------------------------
+	
+	.data
+	.balign 4	
+hello:  .asciz "Hello World!\n\n"
+seconds:
+	.int	1		@ 1 sec
+microsec:
+	.int	500000		@ 0.5 sec
+	
+n	.req    r6              @ counter
+max	.req	r5		@ max # iterations
+
+@ ---------------------------------------
+@	Code Section
+@ ---------------------------------------
+	
+	.text
+	.global main
+	.extern printf
+	.extern sleep
+	.extern usleep
+	
+main:   push 	{ip, lr}	@ push return address + dummy register
+				@ for alignment
+
+	mov	n, #0
+	mov	max, #10
+	
+loop:	cmp	n, max		@ n>max?
+	bgt	done		@ yes, done
+				@ no, output string
+	
+        ldr 	r0, =hello
+        bl 	printf		@ print string and r1 as param
+
+@	ldr	r0, =seconds	@ use sleep for integer # of 
+@	ldr	r0, [r0]	@ seconds
+@	bl	sleep
+
+	ldr	r0, =microsec	@ use usleep for microsecond
+	ldr	r0, [r0]	@ delays.
+	bl	usleep
+	
+	add	n, #1		@ n++
+	b	loop
+
+done:	
+        pop 	{ip, pc}	@ pop return address into pc
