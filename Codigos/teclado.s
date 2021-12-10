48ab356d6af759430b324e8db36579a328af9b75
Jafet Diaz <79779934+jaberwooc@users.noreply.github.com>
2021-12-10 14:49:29 -0800

    Create teclado.s

---
+++ Codigos/teclado.s
@@ -0,0 +1,41 @@
+       .section        .bss
+        .comm buffer, 48             
+        .section        .data
+msg:
+        .ascii  "** Greeter **\nPlease enter your name: "
+        msgLen = . - msg
+msg2:
+        .ascii  "Hello "
+        msg2Len = . - msg2
+
+.section        .text
+.globl  main
+main:
+
+mov r0, $1
+ldr r1, =msg
+ldr r2, =msgLen
+mov r7, $4
+svc $0
+
+mov r7, $3
+mov r0, $1
+ldr r1, =buffer
+mov r2, $0x30
+svc $0
+
+mov r0, $1
+ldr r1, =msg2
+ldr r2, =msg2Len
+mov r7, $4
+svc $0
+
+mov r0, $1
+ldr r1, =buffer
+mov r2, $0x30
+mov r7, $4
+svc $0
+
+mov r7, $1
+svc $0
+.end
